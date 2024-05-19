13d6e260774e2362e3836a6f5e76a1ff8fc63eba
aaron <aaron.rohin@hotmail.co.uk>
2024-05-19 21:05:56 +0100

    change fade in animatioN

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const Intro = () => {
   const fadeInAnimation = useFadeInAnimation();
-  const fadeInAnimationTwo = useFadeInAnimation(0.3);
+  const fadeInAnimationParagraph = useFadeInAnimation(0.3);
 
   return (
     <div className={style.intro}>
@@ -12,20 +12,20 @@ const Intro = () => {
         <motion.h1 {...fadeInAnimation} id="cosmos">
           Cosmos
         </motion.h1>
-        <motion.p {...fadeInAnimationTwo}>
+        <motion.p {...fadeInAnimationParagraph}>
           {`Once you have an innovation culture, even those who are not scientists
           or engineers - poets, actors, journalists - they, as communities,
           embrace the meaning of what it is to be scientifically literate. They
           embrace the concept of an innovation culture. They vote in ways that
           promote it. They don't fight science and they don't fight technology.`}
         </motion.p>
-        <motion.p {...fadeInAnimationTwo}>
+        <motion.p {...fadeInAnimationParagraph}>
           {` Private enterprise in the history of civilization has never led large,
           expensive, dangerous projects with unknown risks. That has never
           happened because when you combine all these factors, you cannot create
           a capital market valuation of that activity.`}
         </motion.p>
-        <motion.p {...fadeInAnimationTwo}>
+        <motion.p {...fadeInAnimationParagraph}>
           {` In science, if you don't do it, somebody else will. Whereas in art, if
           Beethoven didn't compose the 'Ninth Symphony,' no one else before or
           after is going to compose the 'Ninth Symphony' that he composed; no
